46e04cde22a0bff01602d724a3e31156ffa6e896
phuoc sang <sangphanphuoc2301@gmail.com>
2024-03-11 10:59:47 +0700

    Chapter 7: Sign up

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module RailsTutorial
     config.load_defaults 7.0
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.available_locales = [:en, :vi]
-    config.i18n.default_locale = :vi
+    config.i18n.default_locale = :en
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
